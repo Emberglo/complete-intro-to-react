9643bb45b209278929f1ad1365f94eeb9220a65c
Ryan Carpenter <rccarpenter@outlook.com>
2023-07-27 15:35:32 -0600

    router changes

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,5 +1,10 @@
+import { useParams } from "react-router-dom";
+
 const Details = () => {
-  return <h2>hi!</h2>;
+  // id is coming from a store created by the browserrouter in app.js
+  const { id } = useParams();
+
+  return <h2>{id}</h2>;
 };
 
 export default Details;
